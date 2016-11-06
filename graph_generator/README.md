bbc93963da6d9e090016c50963231756d0ee2356
Michal Kostrzewa <kostrzewa.michal@o2.pl>
2016-11-06 18:10:13 +0100

    Random and complete graph generator.

---
+++ graph_generator/README.md
@@ -0,0 +1,13 @@
+## graph generator
+
+This directory contains scripts for generating graphs in adjacency list format,
+e.g.
+
+```
+1 2 3
+2 3 6 7
+3 6 7
+...
+```
+
+Results are saved in test_data directory.
---
+++ graph_generator/gen.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import random
+from datetime import datetime
+from collections import defaultdict
+
+is_complete = int(input("Graph type: 1-complete, 0-random: "))
+num_nodes = int(input("Number of nodes: "))
+
+nodes = defaultdict(list)
+filename = ""
+
+if not is_complete:
+    num_edges = int(input("Number of edges: "))
+    seed = input("Random seed (empty = time-based): ")
+    if seed == "":
+        seed = datetime.now()
+    random.seed(seed)
+
+    edge_idx = 0
+    while edge_idx < num_edges:
+        current_node_idx = random.randint(0, num_nodes)
+        random_node = current_node_idx
+        while random_node == current_node_idx:
+            random_node = random.randint(0, num_nodes)
+        nodes[current_node_idx].append(random_node)
+        edge_idx += 1
+
+    filename = "{}_{}_{}".format(num_nodes, num_edges, seed)
+
+else:
+    for i in range(num_nodes):
+        others = list(range(num_nodes))
+        others.remove(i)
+        nodes[i] = others
+    filename = "K{}".format(num_nodes)
+
+
+filepath = "../test_data/{}".format(filename)
+
+with open(filepath, "w") as f:
+    for k, v in nodes.items():
+        f.write(str(k) + " " + " ".join(map(str, v)) + "\n")
+print("saved results to {}".format(filepath))
